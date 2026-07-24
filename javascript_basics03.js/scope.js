c895b58c4a66e03cff2ff391fce33289d19e0b25
Ujjawal0019 <ujjwalgautam607@gmail.com>
2026-07-24 17:57:02 +0000

    scope done

--- javascript_basics03.js/scope.js
+++ javascript_basics03.js/scope.js
@@ -1,58 +1,65 @@
-//var c = 300
-let a = 300
+// Global Scope
+const globalVar = "I am globally accessible";
+
+function testGlobalScope() {
+  console.log("Inside function accessing global:", globalVar);
+}
+testGlobalScope();
+console.log("Outside function accessing global:", globalVar);
+
+console.log("\n--- Function / Local Scope ---");
+
+function localScopeDemo() {
+  const functionVar = "I am inside a function";
+  console.log("Inside function:", functionVar);
+}
+localScopeDemo();
+
+// console.log(functionVar); // ReferenceError: functionVar is not defined
+
+console.log("\n--- Block Scope (let & const vs var) ---");
+
 if (true) {
-    let a = 10
-    const b = 20
-    // console.log("INNER: ", a);
+  let blockScopedLet = "I am locked in this block";
+  const blockScopedConst = "I am also locked in this block";
+  var functionScopedVar = "I leak out of this block!";
+}
+
+// console.log(blockScopedLet);   // ReferenceError: blockScopedLet is not defined
+// console.log(blockScopedConst); // ReferenceError: blockScopedConst is not defined
+console.log("Accessing var outside block:", functionScopedVar);
+
+console.log("\n--- Scope Chain & Lexical Scope ---");
+
+const outerVar = "Outer scope variable";
+
+function outerFunction() {
+  const innerVar = "Inner scope variable";
+
+  function innerFunction() {
+    const deepestVar = "Deepest scope variable";
     
+    // The scope chain allows inner functions to look upward for variables
+    console.log("Accessing deepest:", deepestVar);
+    console.log("Accessing inner:", innerVar);
+    console.log("Accessing outer:", outerVar);
+  }
+
+  innerFunction();
+  // console.log(deepestVar); // ReferenceError: deepestVar is not defined
 }
 
+outerFunction();
 
+console.log("\n--- Shadowing Variables ---");
 
-// console.log(a);
-// console.log(b);
-// console.log(c);
+const shadowVar = "Global value";
 
-
-function one(){
-    const username = "hitesh"
-
-    function two(){
-        const website = "youtube"
-        console.log(username);
-    }
-    // console.log(website);
-
-     two()
-
+function shadowDemo() {
+  // Local variable "shadows" (overrides) the global variable with the same name
+  const shadowVar = "Local value";
+  console.log("Inside shadow function:", shadowVar);
 }
 
-// one()
-
-if (true) {
-    const username = "hitesh"
-    if (username === "hitesh") {
-        const website = " youtube"
-        // console.log(username + website);
-    }
-    // console.log(website);
-}
-
-// console.log(username);
-
-
-// ++++++++++++++++++ interesting ++++++++++++++++++
-
-
-console.log(addone(5))
-
-function addone(num){
-    return num + 1
-}
-
-
-
-addTwo(5)
-const addTwo = function(num){
-    return num + 2
-}
+shadowDemo();
+console.log("Outside shadow function:", shadowVar);
