5d96fc83fdd571080751da8140900a661112092d
Ujjawal0019 <ujjwalgautam607@gmail.com>
2026-07-24 17:56:12 +0000

    arrow done

--- javascript_basics03.js/arrow.js
+++ javascript_basics03.js/arrow.js
@@ -1,54 +1,53 @@
-const user = {
-    username: "hitesh",
-    price: 999,
-
-    welcomeMessage: function() {
-        console.log(`${this.username} , welcome to website`);
-        console.log(this);
-    }
-
+// Basic Arrow Function vs Traditional Function
+function traditionalAdd(a, b) {
+  return a + b;
 }
 
-// user.welcomeMessage()
-// user.username = "sam"
-// user.welcomeMessage()
+const arrowAdd = (a, b) => {
+  return a + b;
+};
 
-// console.log(this);
+console.log("Traditional Add:", traditionalAdd(2, 3));
+console.log("Arrow Add:", arrowAdd(2, 3));
 
-// function chai(){
-//     let username = "hitesh"
-//     console.log(this.username);
-// }
+// Implicit Return (Concise Body)
+const multiply = (a, b) => a * b;
+const square = x => x * x; 
+const sayHello = () => "Hello World!";
 
-// chai()
+console.log("Implicit Multiply:", multiply(4, 5));
+console.log("Implicit Square:", square(6));
+console.log("No Parameters:", sayHello());
 
-// const chai = function () {
-//     let username = "hitesh"
-//     console.log(this.username);
-// }
+// Returning Object Literals Implicitly (wrapped in parentheses)
+const createUser = (name, role) => ({ name: name, role: role });
+console.log("Implicit Object Return:", createUser("Alice", "Admin"));
 
-const chai =  () => {
-    let username = "hitesh"
-    console.log(this);
-}
+// Arrow Functions as Callbacks
+const numbers = [1, 2, 3, 4, 5];
+const doubled = numbers.map(num => num * 2);
+const evens = numbers.filter(num => num % 2 === 0);
 
+console.log("Doubled Array:", doubled);
+console.log("Filtered Evens:", evens);
 
-// chai()
+// Lexical 'this' Binding (Arrow functions do not have their own 'this')
+const timer = {
+  seconds: 0,
+  start() {
+    // Arrow function retains 'this' from the start() method context
+    setTimeout(() => {
+      this.seconds++;
+      console.log("Timer seconds:", this.seconds);
+    }, 100);
+  }
+};
 
-// const addTwo = (num1, num2) => {
-//     return num1 + num2
-// }
+timer.start();
 
-// const addTwo = (num1, num2) =>  num1 + num2
+// No 'arguments' object in arrow functions (Use Rest Parameters instead)
+const sumAll = (...args) => {
+  return args.reduce((total, current) => total + current, 0);
+};
 
-// const addTwo = (num1, num2) => ( num1 + num2 )
-
-const addTwo = (num1, num2) => ({username: "hitesh"})
-
-
-console.log(addTwo(3, 4))
-
-
-// const myArray = [2, 5, 3, 7, 8]
-
-// myArray.forEach()
+console.log("Sum with Rest Parameters:", sumAll(10, 20, 30, 40));
