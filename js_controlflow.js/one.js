c0a065ccbfa347d4495694b0a3016d51d1e939d1
Ujjawal0019 <ujjwalgautam607@gmail.com>
2026-07-25 18:08:40 +0000

    controll flow one done

--- js_controlflow.js/one.js
+++ js_controlflow.js/one.js
@@ -1,52 +1,68 @@
-// if
-const isUserloggedIn = true
-const temperature = 41
+// If, Else If, Else statements
+const age = 20;
 
-// if ( temperature === 40 ){
-//     console.log("less than 50");
-// } else {
-//     console.log("temperature is greater than 50");
-// }
-
-// console.log("Execute");
-// <, >, <=, >=, ==, !=, ===, !==
-
-// const score = 200
-
-// if (score > 100) {
-//     let power = "fly"
-//     console.log(`User power: ${power}`);
-// }
-
-// console.log(`User power: ${power}`);
-
-
-// const balance = 1000
-
-// if (balance > 500) console.log("test"),console.log("test2");
-
-// if (balance < 500) {
-//     console.log("less than 500");
-// } else if (balance < 750) {
-//     console.log("less than 750");
-    
-// } else if (balance < 900) {
-//     console.log("less than 750");
-    
-// } else {
-//     console.log("less than 1200");
-
-// }
-
-const userLoggedIn = true
-const debitCard = true
-const loggedInFromGoogle = false
-const loggedInFromEmail = true
-
-if (userLoggedIn && debitCard && 2==3) {
-    console.log("Allow to buy course");
+if (age < 13) {
+  console.log("Child");
+} else if (age >= 13 && age < 20) {
+  console.log("Teenager");
+} else {
+  console.log("Adult");
 }
 
-if (loggedInFromGoogle || loggedInFromEmail) {
-    console.log("User logged in");
+// Ternary Operator (Shorthand for simple if/else)
+const isRaining = true;
+const action = isRaining ? "Take an umbrella" : "Wear sunglasses";
+console.log("Weather action:", action);
+
+// Switch Statement
+const dayOfWeek = 3;
+let dayName;
+
+switch (dayOfWeek) {
+  case 1:
+    dayName = "Monday";
+    break;
+  case 2:
+    dayName = "Tuesday";
+    break;
+  case 3:
+    dayName = "Wednesday";
+    break;
+  default:
+    dayName = "Unknown Day";
+}
+console.log("Day:", dayName);
+
+// For Loop
+for (let i = 0; i < 3; i++) {
+  console.log("For loop iteration:", i);
+}
+
+// While Loop
+let counter = 0;
+while (counter < 3) {
+  console.log("While loop iteration:", counter);
+  counter++;
+}
+
+// Do...While Loop (Always executes at least once)
+let doCounter = 0;
+do {
+  console.log("Do...While iteration:", doCounter);
+  doCounter++;
+} while (doCounter < 3);
+
+// Break and Continue
+for (let i = 0; i < 5; i++) {
+  if (i === 2) {
+    console.log("Skipping iteration 2 using continue");
+    continue; 
+  }
+  
+  if (i === 4) {
+    console.log("Stopping loop completely at 4 using break");
+    break; 
+  }
+  
+  console.log("Loop iteration:", i);
 }
